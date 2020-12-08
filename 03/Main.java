44c34aa48a50c2f39801f6a0664c232e0aaceb6a
rexim <reximkut@gmail.com>
2020-12-08 15:44:51 +0700

    [03] clean up

--- 03/Main.java
+++ 03/Main.java
@@ -22,15 +22,11 @@ public class Main {
         return result;
     }
 
-    public static void main(String[] args) throws IOException {
-        String[] input = null;
-        try(var file = new BufferedReader(new FileReader("input.txt"))) {
-            input = file
-                .lines()
-                .collect(Collectors.toList())
-                .toArray(new String[0]);
-        }
+    static long part1(String[] input) {
+        return countTrees(input, 1, 3);
+    }
 
+    static long part2(String[] input) {
         int slopes[][] = {
             {1, 1},
             {1, 3},
@@ -43,6 +39,27 @@ public class Main {
         for (int i = 0; i < slopes.length; ++i) {
             result *= countTrees(input, slopes[i][0], slopes[i][1]);
         }
-        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length <= 0) {
+            System.err.println("Input file is not provided");
+            System.exit(1);
+        }
+
+        var filepath = args[0];
+
+        String[] input = null;
+        try(var file = new BufferedReader(new FileReader(filepath))) {
+            input = file
+                .lines()
+                .collect(Collectors.toList())
+                .toArray(new String[0]);
+        }
+
+        System.out.printf("Input file: %s\n", filepath);
+        System.out.printf("Part 1: %d\n", part1(input));
+        System.out.printf("Part 2: %d\n", part2(input));
     }
 }
---
+++ 03/README.md
@@ -0,0 +1,6 @@
+# [Day 3](https://adventofcode.com/2020/day/3) solution in [Java](https://www.oracle.com/java/)
+
+## Quick Start
+
+- install [JDK](https://www.oracle.com/java/technologies/javase-downloads.html) and make sure `javac` and `java` are avaialble in `$PATH`
+- `$ make`
