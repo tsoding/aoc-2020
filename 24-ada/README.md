e8702608f0ff98b872b4f69a145caf41e5686f7e
rexim <reximkut@gmail.com>
2020-12-25 22:07:18 +0700

    [24] Add part 2

---
+++ 24-ada/README.md
@@ -0,0 +1,24 @@
+# [Day 24](https://adventofcode.com/2020/day/24) solution in [Ada](https://www.adaic.org/)
+
+## Tested on 
+
+```console
+$ gnat --version
+GNAT 8.3.0
+Copyright (C) 1996-2018, Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.
+There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+```
+
+## Expected Result
+
+```console
+Input file: input.txt
+  Part 1: 360
+  Part 2: 3924
+```
+
+## Quick Start
+
+- install [GNAT](https://gcc.gnu.org/wiki/GNAT) and make sure `gnatmake` is available in `$PATH`
+- `$ make`
--- 24-ada/day24.adb
+++ 24-ada/day24.adb
@@ -12,45 +12,6 @@ procedure Day24 is
         Z : Integer;
     end record;
 
-    function Tile_Hash(T: Tile) return Hash_Type is
-    begin
-        return Hash_Type(T.X + T.Y + T.Z);
-    end;
-
-    package Floor is new Ada.Containers.Hashed_Maps
-        (Key_Type => Tile,
-         Element_Type => Boolean,
-         Hash => Tile_Hash,
-         Equivalent_Keys => "=");
-
-    procedure Flip_Tile(F: in out Floor.Map; T: Tile) is
-        D: Floor.Cursor := Floor.Find(F, T);
-    begin
-        if not Floor.Has_Element(D) then
-            Floor.Insert(F, T, True);
-        else
-            Floor.Replace_Element(F, D, not Floor.Element(D));
-        end if;
-    end;
-
-    function Count_Black(F: in Floor.Map) return Integer is
-        C: Floor.Cursor := Floor.First(F);
-        Result: Integer := 0;
-    begin
-        while Floor.Has_Element(C) loop
-            if Floor.Element(C) then
-                Result := Result + 1;
-            end if;
-            C := Floor.Next(C);
-        end loop;
-        return Result;
-    end;
-
-    function Tile_Image(T: Tile) return String is
-    begin
-        return "(" & Integer'Image(T.X) & ", " & Integer'Image(T.Y) & ", " & Integer'Image(T.Z) & ")";
-    end;
-
     function Tile_Add(T1: Tile; T2: Tile) return Tile is
     begin
         return (T1.X + T2.X, T1.Y + T2.Y, T1.Z + T2.Z);
@@ -73,6 +34,94 @@ procedure Day24 is
         (X =>  0, Y =>  1, Z => -1), --("nw"),
         (X =>  1, Y =>  0, Z => -1)  --("ne")
     );
+
+
+    function Tile_Hash(T: Tile) return Hash_Type is
+        Result : Long_Integer := Long_Integer(T.X);
+    begin
+        Result := Result * 31 + Long_Integer(T.Y);
+        Result := Result * 97 + Long_Integer(T.Z);
+        return Hash_Type(Result mod 1_000_000_000);
+    end;
+
+    package Floor is new Ada.Containers.Hashed_Maps
+        (Key_Type => Tile,
+         Element_Type => Boolean,
+         Hash => Tile_Hash,
+         Equivalent_Keys => "=");
+
+    procedure Flip_Tile(F: in out Floor.Map; T: Tile) is
+        D: Floor.Cursor := Floor.Find(F, T);
+    begin
+        if not Floor.Has_Element(D) then
+            Floor.Insert(F, T, True);
+        else
+            Floor.Replace_Element(F, D, not Floor.Element(D));
+        end if;
+    end;
+
+    function Is_Black(F: in Floor.Map; T: Tile) return Boolean is
+        C: Floor.Cursor := Floor.Find(F, T);
+    begin
+        return Floor.Has_Element(C) and then Floor.Element(C);
+    end;
+
+    function Count_Neighbours(F: in Floor.Map; T: Tile) return Integer is
+        Result: Integer := 0;
+    begin
+        for Dir of Dirs loop
+            if Is_Black(F, Tile_Add(T, Dir)) then
+                Result := Result + 1;
+            end if;
+        end loop;
+        return Result;
+    end;
+
+    procedure Set_Tile(F: in out Floor.Map; T: Tile; Value: Boolean) is
+        C: Floor.Cursor := Floor.Find(F, T);
+    begin
+        if Floor.Has_Element(C) then
+            Floor.Replace_Element(F, C, Value);
+        else
+            Floor.Insert(F, T, Value);
+        end if;
+    end;
+
+    procedure Next_Floor(F1: in Floor.Map; F2: out Floor.Map) is
+        T : Tile;
+        Neighbours : Integer;
+    begin
+        Floor.Clear(F2);
+        for C in Floor.Iterate(F1) loop
+            for Dir of Dirs loop
+                T := Tile_Add(Floor.Key(C), Dir);
+                Neighbours := Count_Neighbours(F1, T);
+                if Is_Black(F1, T) then
+                    Set_Tile(F2, T, not (Neighbours = 0 or Neighbours > 2));
+                else
+                    Set_Tile(F2, T, Neighbours = 2);
+                end if;
+            end loop;
+        end loop;
+    end;
+
+    function Count_Black(F: in Floor.Map) return Integer is
+        C: Floor.Cursor := Floor.First(F);
+        Result: Integer := 0;
+    begin
+        while Floor.Has_Element(C) loop
+            if Floor.Element(C) then
+                Result := Result + 1;
+            end if;
+            C := Floor.Next(C);
+        end loop;
+        return Result;
+    end;
+
+    function Tile_Image(T: Tile) return String is
+    begin
+        return "(" & Integer'Image(T.X) & ", " & Integer'Image(T.Y) & ", " & Integer'Image(T.Z) & ")";
+    end;
 
     function Dir_To_Delta(Dir_Name: Unbounded_String) return Tile is
     begin
@@ -111,12 +160,10 @@ procedure Day24 is
         return Result;
     end Parse_Tile;
 
-    procedure Solve_File(File_Path: String) is
+    procedure Floor_From_File(File_Path: String; F: out Floor.Map) is
         File : File_Type;
         T : Tile;
-        F : Floor.Map;
     begin
-        Put_Line("Input file: " & File_Path);
         Open(File => File,
              Mode => In_File,
              Name => File_Path);
@@ -124,7 +171,33 @@ procedure Day24 is
             T := Parse_Tile(To_Unbounded_String(Get_Line(File)));
             Flip_Tile(F, T);
         end loop;
-        Put_Line(Integer'Image(Count_Black(F)));
+        Close(File);
+    end;
+
+    function Part1(File_Path: String) return Integer is
+        F : Floor.Map;
+    begin
+        Floor_From_File(File_Path, F);
+        return Count_Black(F);
+    end;
+
+    function Part2(File_Path: String) return Integer is
+        F : array (0..1) of Floor.Map;
+        Current : Integer := 0;
+    begin
+        Floor_From_File(File_Path, F(Current));
+        for i in 1..100 loop
+            Next_Floor(F(Current), F(1 - Current));
+            Current := 1 - Current;
+        end loop;
+        return Count_Black(F(Current));
+    end;
+
+    procedure Solve_File(File_Path: String) is
+    begin
+        Put_Line("Input file: " & File_Path);
+        Put_Line("  Part 1:" & Integer'Image(Part1(File_Path)));
+        Put_Line("  Part 2:" & Integer'Image(Part2(File_Path)));
     end Solve_File;
 
 begin
