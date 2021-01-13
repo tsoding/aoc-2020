91eb7a68b4472c004d8871916c437fc6c3f7c890
Fabien Chouteau <chouteau@adacore.com>
2021-01-13 15:45:06 +0100

    Use an enum for directions

--- 24-ada/day24.adb
+++ 24-ada/day24.adb
@@ -3,6 +3,7 @@ with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with System.Assertions; use System.Assertions;
 with Ada.Containers.Hashed_Maps; use Ada.Containers;
+with Ada.Characters.Handling; use Ada.Characters.Handling;
 
 procedure Day24 is
 
@@ -17,24 +18,16 @@ procedure Day24 is
         return (T1.X + T2.X, T1.Y + T2.Y, T1.Z + T2.Z);
     end;
 
-    Dir_Names : constant array (1..6) of Unbounded_String := (
-        To_Unbounded_String("e"), 
-        To_Unbounded_String("se"), 
-        To_Unbounded_String("sw"), 
-        To_Unbounded_String("w"),
-        To_Unbounded_String("nw"),
-        To_Unbounded_String("ne")
-    );
+    type Dir_Kind is (E, SE, SW, W, NW, NE);
 
-    Dirs : constant array (1..6) of Tile := (
-        (X =>  1, Y => -1, Z =>  0), --("e"), 
-        (X =>  0, Y => -1, Z =>  1), --("se"), 
-        (X => -1, Y =>  0, Z =>  1), --("sw"), 
-        (X => -1, Y =>  1, Z =>  0), --("w"),
-        (X =>  0, Y =>  1, Z => -1), --("nw"),
-        (X =>  1, Y =>  0, Z => -1)  --("ne")
+    Dir_To_Delta : constant array (Dir_Kind) of Tile := (
+       E   => (X =>  1, Y => -1, Z =>  0),
+       SE  => (X =>  0, Y => -1, Z =>  1),
+       SW  => (X => -1, Y =>  0, Z =>  1),
+       W   => (X => -1, Y =>  1, Z =>  0),
+       NW  => (X =>  0, Y =>  1, Z => -1),
+       NE  => (X =>  1, Y =>  0, Z => -1)
     );
-
 
     function Tile_Hash(T: Tile) return Hash_Type is
         Result : Long_Integer := Long_Integer(T.X);
@@ -69,7 +62,7 @@ procedure Day24 is
     function Count_Neighbours(F: in Floor.Map; T: Tile) return Integer is
         Result: Integer := 0;
     begin
-        for Dir of Dirs loop
+        for Dir of Dir_To_Delta loop
             if Is_Black(F, Tile_Add(T, Dir)) then
                 Result := Result + 1;
             end if;
@@ -93,7 +86,7 @@ procedure Day24 is
     begin
         Floor.Clear(F2);
         for C in Floor.Iterate(F1) loop
-            for Dir of Dirs loop
+            for Dir of Dir_To_Delta loop
                 T := Tile_Add(Floor.Key(C), Dir);
                 Neighbours := Count_Neighbours(F1, T);
                 if Is_Black(F1, T) then
@@ -123,24 +116,18 @@ procedure Day24 is
         return "(" & Integer'Image(T.X) & ", " & Integer'Image(T.Y) & ", " & Integer'Image(T.Z) & ")";
     end;
 
-    function Dir_To_Delta(Dir_Name: Unbounded_String) return Tile is
+    function Next_Dir(Desc: String) return Dir_Kind is
     begin
-        for Index in Dir_Names'Range loop
-            if Dir_Name = Dir_Names(Index) then
-                return Dirs(Index);
-            end if;
-        end loop;
-        Raise_Assert_Failure("Unknown direction `" & To_String(Dir_Name) & "`");
-    end Dir_To_Delta;
-
-    function Next_Dir(Desc: Unbounded_String) return Unbounded_String is
-    begin
-        for Dir of Dir_Names loop
-            if Length(Dir) <= Length(Desc) then
-                if Slice(Desc, 1, Length(Dir)) = Dir then
+        for Dir in Dir_Kind loop
+           declare
+              Dir_Str : constant String := To_Lower (Dir'Img);
+           begin
+              if Dir_Str'Length <= Desc'Length then
+                 if Desc (Desc'First .. Desc'First + Dir_Str'Length - 1) = Dir_Str then
                     return Dir;
-                end if;
-            end if;
+                 end if;
+              end if;
+           end;
         end loop;
 
         Raise_Assert_Failure("Unreachable. Could not get the next direction");
@@ -148,13 +135,13 @@ procedure Day24 is
 
     function Parse_Tile(Desc: Unbounded_String) return Tile is
         Result : Tile := (X => 0, Y => 0, Z => 0);
-        Dir: Unbounded_String;
+        Dir: Dir_Kind;
         Input: Unbounded_String := Desc;
     begin
         while Length(Input) > 0 loop
-            Dir := Next_Dir(Input);
-            Input := Unbounded_Slice(Input, Length(Dir) + 1, Length(Input));
-            Result := Tile_Add(Result, Dir_To_Delta(Dir));
+            Dir := Next_Dir(To_String (Input));
+            Input := Unbounded_Slice(Input, Dir'Img'Length + 1, Length(Input));
+            Result := Tile_Add(Result, Dir_To_Delta (Dir));
         end loop;
 
         return Result;
